2de03346a7c5336ccc3bda46a8c9ae52097805e1
esong501 <y0501esong@gmail.com>
2022-11-11 11:20:02 -0500

    api access token bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {Buffer} from 'buffer';
 
 function App() {
+
+  var client_id = 'CLIENT_ID';
+  var client_secret = 'CLIENT_SECRET';
+
+  // var authOptions = {
+  //   {
+  //     grant_type: 'client_credentials'
+  //   }
+  // };
+
+  // request.post(authOptions, function(error, response, body) {
+  //   if (!error && response.statusCode === 200) {
+  //     var token = body.access_token;
+  //   }
+  // });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + (Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    body: {
+      grant_type: 'client_credentials'
+    }
+  };
+  fetch('https://accounts.spotify.com/api/token', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log({ postId: data.id }));
+
+
   return (
     <div className="App">
+      {/* {console.log(process.env.REACT_APP_TITLE)} */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +49,8 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+        </div>
       </header>
     </div>
   );
