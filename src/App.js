fe4a6545bffc6bb88d73a05c4d233c49e8fa9eb3
MohamedADirie <mdirie@bu.edu>
2022-12-07 23:47:13 -0500

    Route to Travel calendar installed with auth0 landing page. Landing page needs an api key from Google but otherwise good

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 import {CssBaseline, Grid} from '@mui/material';
 import { getPlacesData } from './api';
 
+import Login from './components/LoginLogoutAuth0/Login'
+import Logout from './components/LoginLogoutAuth0/Logout'
+import LoginLogout from './components/LoginLogoutAuth0/LoginLogout'
+
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Planner from './components/Planner/Planner';
+
+import { Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 //test
 function App() {
   const [type, setType] = useState('attractions');
@@ -22,25 +29,30 @@ function App() {
        })
   }, [type, coordinates, bounds]);
 
-  return (
-   <>
-   <CssBaseline />
-   <Header 
-    setCoordinates={setCoordinates} 
-    setBounds={setBounds}
-    />
-   <Grid container spacing={3} style={{width :'100%'}}>
-    <Grid item xs={12} md = {5} >
-      <List 
-      places ={places}
-      type = {type}
-      setType={setType}/>
-    </Grid>
-    <Grid item xs={12} md= {7} style={{ display: 'flex'}}>
-      <Planner/>
-    </Grid>
-   </Grid>
-   </>
+  return(
+    <Routes>
+      <Route path="/" element={<LoginLogout />} />
+
+      <Route path="/travel" element={<div><>
+                                             <CssBaseline />
+                                             <Header
+                                              setCoordinates={setCoordinates}
+                                              setBounds={setBounds}
+                                              />
+                                             <Grid container spacing={3} style={{width :'100%'}}>
+                                              <Grid item xs={12} md = {5} >
+                                                <List
+                                                places ={places}
+                                                type = {type}
+                                                setType={setType}/>
+                                              </Grid>
+                                              <Grid item xs={12} md= {7} style={{ display: 'flex'}}>
+                                                <Planner/>
+                                              </Grid>
+                                             </Grid>
+                                             </><Logout/ ></div>} />
+
+    </Routes>
   );
 }
 
