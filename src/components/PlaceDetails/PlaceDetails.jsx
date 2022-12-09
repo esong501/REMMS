b1e7ee72237de19f2651d62f6810bcf5d1ed3c46
MohamedADirie <mdirie@bu.edu>
2022-12-09 02:27:01 -0500

    Fixed PlaceDetails.jsx and demo version ready for Friday with auth0 and travel stuff

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,47 +1,21 @@
-import React, {useState, useEffect} from 'react';
-import '../../App.css';
-import {CssBaseline, Grid} from '@mui/material';
-import { getPlacesData } from '../../api';
+import React from 'react';
+import{Box, Typography, Button, Card, CardMedia, CardActions, Chip, CardContent} from '@mui/material';
 
-import Header from '../Header/Header';
-import List from '../List/List';
-import Planner from '../Planner/Planner';
 
-function App() {
-  const [type, setType] = useState('attractions');
-  const [places, setPlaces] = useState([]);
 
-  const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState({});
-  
-  useEffect(() => {
-     getPlacesData(type, bounds.bl_lat, bounds.tr_lat, bounds.bl_long, bounds.tr_long)
-       .then((data) =>{
-         console.log(data)
-         setPlaces(data)
-       })
-  }, [type, coordinates, bounds]);
+const PlaceDetails = ({place}) => {
 
-  return (
-   <>
-   <CssBaseline />
-   <Header 
-    setCoordinates={setCoordinates} 
-    setBounds={setBounds}
-    />
-   <Grid container spacing={3} style={{width :'100%'}}>
-    <Grid item xs={12} md = {5} >
-      <List 
-      places ={places}
-      type = {type}
-      setType={setType}/>
-    </Grid>
-    <Grid item xs={12} md= {7} style={{ display: 'flex'}}>
-      <Planner/>
-    </Grid>
-   </Grid>
-   </>
-  );
+    return(
+        <Card elevation={6}>
+            <CardMedia style={{height:350}}
+            image =  {place.photo ? place.photo.images.large.url :'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+            title= {place.name}
+        />
+        <CardContent>
+            <Typography gutterBottom variant='h5'>{place.name}</Typography>
+        </CardContent>
+        </Card>
+    );
 }
 
-export default App;
+export default PlaceDetails;
