a4427ca2f2c411854232210b074299ffc42d2e32
MohamedADirie <mdirie@bu.edu>
2022-12-09 11:20:29 -0500

    Made Logout Button look good

--- src/components/LoginLogoutAuth0/Logout.js
+++ src/components/LoginLogoutAuth0/Logout.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import './Logout.css'
+
 // krate_id = 04bf7fe9abd9c04a13d3
 export default function Logout(){
     const { logout } = useAuth0();
     return(
-        <button onClick={() => logout({ returnTo: window.location.origin,})}>Logout </button>
+        <button class="btn draw-border" onClick={() => logout({ returnTo: window.location.origin,})}>Logout </button>
     );
 }
